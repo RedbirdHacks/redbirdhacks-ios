b0776790d9845c07046c4df4dd80b26fe6b3cba0
zzt4 <sonic.boom.4@gmail.com>
2014-10-19 21:33:11 -0500

    Initial Commit

---
+++ RedbirdHacks/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RedbirdHacks
+//
+//  Created by Tallyn Turnbow on 10/19/14.
+//  Copyright (c) 2014 Tallyn Turnbow. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
